8c8c311f98ec1304bc40c6f90f41b25f0a8f712e
Matt Pityo <mpityo@gmail.com>
2022-02-08 17:39:45 -0500

    add all food filtering to seperate js file, and finish error handling

---
+++ assets/js/foodFilterHandler.js
@@ -0,0 +1,76 @@
+var foodAddedEl = document.querySelector("#food-added");
+var foodOptionsSelected = [];
+var listOfFoodOptions = ['Thai', 'Italian', 'French', 'American', 'Artistic', 'Mexican'];
+
+var deleteFoodType = function (typeToRemove) {  
+    // add food name back to food selection list
+    var foodName = typeToRemove.target.parentElement.firstChild.data;
+    createFoodTypeEl(foodName);
+
+    // remove food from the list of food that has already been selected
+    typeToRemove.target.closest("span").remove();
+
+    // loop through the array with the food being passed to API and remove from there as well
+    for (var i = 0; i < foodOptionsSelected.length; i++) {
+        if (foodOptionsSelected[i] === foodName) {
+            foodOptionsSelected.splice(i, 1);
+            break;
+        }
+    }
+}
+
+var createFoodTypeEl = function (foodName) {
+    // create the food cusine type drop down for user to select
+    // get the element to add the options to
+    var dataListEl = formEl.querySelector("datalist");
+
+    // create an option and append to datalist (dataListEl)
+    var optionEl = document.createElement("option");
+    optionEl.id = foodName;
+    optionEl.value = foodName;
+    optionEl.name = "food-types";
+
+    dataListEl.appendChild(optionEl);
+}
+
+var createFoodUserButtons = function (foodName) {
+    // add food type to page for easy removal by user
+    // hold name and delete button in one div
+    var foodAddedContainer = document.createElement("div");
+
+    var foodToAddEl = document.createElement("span");
+    foodToAddEl.textContent = foodName;
+    var deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.name = "delete-food-type";
+
+    foodToAddEl.appendChild(deleteBtn);
+    foodAddedContainer.appendChild(foodToAddEl);
+    foodAddedEl.appendChild(foodAddedContainer);
+
+    // remove the food from the list, so it cannot be selected again
+    document.querySelector("#" + foodName).remove();
+
+    // return the form back to default
+    document.querySelector("#food-input").textContent = "";
+}
+
+function selectFood () {
+    // when a food is selected from the drop down list
+    var foodName = document.getElementById("food-input").value;
+    var foodOptions = document.getElementById('cuisine-types').childNodes;
+
+    for (var i = 0; i < foodOptions.length; i++) {
+        // an item was selected
+        if (foodOptions[i].value === foodName) {
+            // add it to an array
+            if (foodOptionsSelected.length > 0) {
+                foodOptionsSelected.push(foodName);
+            } else {
+                foodOptionsSelected = [foodName];
+            }
+            createFoodUserButtons(foodName);
+            break;
+        }
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,15 @@
 var formEl = document.querySelector("#form");
-var foodAddedEl = document.querySelector("#food-added");
-var foodOptionsSelected = [];
 
-// create the food cusine type drop down for user to select
-var createFoodTypeEl = function () {
-    var listOfFoodOptions = ['Thai', 'Italian', 'French', 'American', 'Artistic', 'Mexican'];
-    // get the element to add the options to
-    var dataListEl = formEl.querySelector("datalist");
-
-    // for each food option type, create an option and append to datalist (dataListEl)
-    listOfFoodOptions.forEach(element => {
-        var optionEl = document.createElement("option");
-        optionEl.id = element;
-        optionEl.value = element;
-        optionEl.name = "food-types";
-
-        dataListEl.appendChild(optionEl);
-    });
-}
-
-function selectFood () {
-    var foodName = document.getElementById("food-input").value;
-    var foodOptions = document.getElementById('cuisine-types').childNodes;
-
-    for (var i = 0; i < foodOptions.length; i++) {
-        if (foodOptions[i].value === foodName) {
-            // an item was selected
-            // add it to an array
-            if (foodOptionsSelected.length > 0) {
-                foodOptionsSelected.push(foodName);
-            } else {
-                foodOptionsSelected = [foodName];
-            }
-            // add food type to page for easy removal
-            var foodAddedContainer = document.createElement("div");
-            var foodToAddEl = document.createElement("span");
-            foodToAddEl.textContent = foodName;
-            var deleteBtn = document.createElement("button");
-            deleteBtn.textContent = "X";
-            foodToAddEl.appendChild(deleteBtn);
-            foodAddedContainer.appendChild(foodToAddEl);
-            foodAddedEl.appendChild(foodAddedContainer);
-
-            // remove the food from the list, so it cannot be selected again
-            document.querySelector("#" + foodName).remove();
-
-            break;
-        }
+var buttonHandler = function (e) {
+    // form submit button is pressed
+    if (e.target.id === "submit-form") {
+        var foodToFind = getSearchStatus();
+        console.log(foodToFind);
+    } else
+    // delete food type that was selected from appearing in results
+    if (e.target.name === "delete-food-type") {
+        // contained in foodFilterHandler.js
+        deleteFoodType(e);
     }
 }
 
@@ -65,15 +28,18 @@ var getSearchStatus = function () {
     return searchConditions;
 }
 
-formEl.querySelector("button").addEventListener("click", function() {  
-    event.preventDefault();
-    var foodToFind = getSearchStatus();
-    console.log(foodToFind);
-});
-
-formEl.querySelector("input").addEventListener("submit", function(e) {
+// handle actions from various buttons that are clicked
+formEl.querySelector("button").addEventListener("click", function(e) {  
     e.preventDefault();
-    console.log("prevented default");
+    buttonHandler(e);
+});
+foodAddedEl.addEventListener("click", function(e) {
+    e.preventDefault();
+    buttonHandler(e);
 });
 
-createFoodTypeEl();
+// populate the list of food that can be filtered by user
+// contained in foodFilterHandler.js
+listOfFoodOptions.forEach(element => {
+    createFoodTypeEl(element);
+});
